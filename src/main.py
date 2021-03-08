fc27b5670c2046a4d7f13140667d3c319259d8b6
pinkfloydfan <jxchen98@gmail.com>
2021-03-08 16:11:44 +0000

    sorted out tornado import issue

---
+++ src/main.py
@@ -0,0 +1,17 @@
+
+import tornado
+from tornado import httpserver, httpclient, ioloop, web, websocket, gen
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
