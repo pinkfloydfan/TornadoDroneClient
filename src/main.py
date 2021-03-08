2b0c331502a505f459199bd2686d762bf4bb5f0e
pinkfloydfan <jxchen98@gmail.com>
2021-03-08 21:24:55 +0000

    a day of unsuccessful experimentation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,29 @@
-
+#ws://192.168.4.22:80
 import tornado
 from tornado import httpserver, httpclient, ioloop, web, websocket, gen
+from tornado.ioloop import PeriodicCallback
+from tornado.websocket import websocket_connect
 
-class MainHandler(tornado.web.RequestHandler):
+
+import websockets
+import asyncio
+
+
+
+class MainHandler(tornado.websocket.WebSocketHandler):
     def get(self):
         self.write("Hello, world")
+
+    async def hello(self):
+        uri = "ws://192.168.4.22:80"
+        async with websockets.connect(uri) as websocket:
+
+            msg = await websocket.recv()
+            print(msg)
+
+
+
+
 
 def make_app():
     return tornado.web.Application([
@@ -13,5 +32,7 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888)
+    app.listen(3000)
     tornado.ioloop.IOLoop.current().start()
+
+  
